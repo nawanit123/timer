53ca1d0286da40e6ecf529bbba6e3d675045bd6d
Nawanit <sharma.nawanit123@gmail.com>
2020-04-15 13:11:06 +0530

    fixed a bug with the duration

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ const circle = document.querySelector("circle");
 const perimeter = circle.getAttribute("r")*2*Math.PI;
 circle.setAttribute("stroke-dasharray",perimeter);
 
-let duration;
+let duration = durationInput.value; 
+ 
+durationInput.addEventListener('change', () => {
+  duration = durationInput.value
+});
 const timer = new Timer(durationInput,startButton,pauseButton,{
     onStart(totalDuration){
-        duration = totalDuration;
+        // duration = totalDuration;
     },
     onTick(timeRemaining){
         circle.setAttribute("stroke-dashoffset",
