982dc05885cc7b8f18630d38ee364ab37f3dd328
Nawanit <sharma.nawanit123@gmail.com>
2020-09-27 17:15:59 +0530

    Porting Form Event Listener to class

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import Timer from './Timer.js';
 
-const durationInput = document.querySelector('#duration');
+const formInput = document.querySelector('form');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 const circle = document.querySelector('circle');
+let inputVal = "";
+let mins,sec,totalTimeinSeconds;
+formInput.addEventListener("submit",(e)=>{
+  const formData = new FormData(e.target);
+  e.preventDefault();
+  inputVal = formData.get("timerValues");
+  mins = parseFloat(inputVal.slice(0,2));
+  sec = parseFloat(inputVal.slice(3))
+  totalTimeinSeconds = mins *60 + sec
+})
 
 const perimeter = circle.getAttribute('r') * 2 * Math.PI;
 circle.setAttribute('stroke-dasharray', perimeter);
