e7e4dca11317aa92aa8917081f6de48be0e1e748
Nawanit <sharma.nawanit123@gmail.com>
2020-09-30 21:18:24 +0530

    Added limitation on Minutes to accept total of 2 digits

--- timer.js
+++ timer.js
@@ -1,9 +1,9 @@
 class Timer {
-  constructor(durationInput,formInput, startButton, pauseButton, callbacks) {
-    this.durationInput = durationInput
+  constructor(durationInput, formInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
     this.formInput = formInput;
     this.totalTimeInSeconds = null;
-    this.duration=null;
+    this.duration = null;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
     this.running = false;
@@ -12,29 +12,28 @@ class Timer {
       this.onTick = callbacks.onTick;
       this.onComplete = callbacks.onComplete;
     }
-   
-      this.formInput.addEventListener("submit",(e)=>{
-        e.preventDefault();
-        let mins,seconds;
-        let inputVal = "";
-        const formData = new FormData(e.target);
-        inputVal = formData.get("timerValues");
-        mins = parseFloat(inputVal.slice(0,2));
-        seconds = parseFloat(inputVal.slice(3))
-        this.totalTimeInSeconds = mins *60 + seconds;
-        this.duration = mins *60 + seconds;
-        this.start();
-      })
 
-  
+    this.formInput.addEventListener('submit', (e) => {
+      e.preventDefault();
+      let mins, seconds;
+      let inputVal = '';
+      const formData = new FormData(e.target);
+      inputVal = formData.get('timerValues');
+      mins = parseFloat(inputVal.slice(0, 2));
+      seconds = parseFloat(inputVal.slice(3));
+      this.totalTimeInSeconds = mins * 60 + seconds;
+      this.duration = mins * 60 + seconds;
+      this.start();
+    });
 
     this.startButton.addEventListener('click', this.start);
     this.pauseButton.addEventListener('click', this.pause);
   }
 
   start = () => {
-    if(this.totalTimeInSeconds >0){
-      if (this.onStart && this.duration ===this.totalTimeInSeconds  ) this.onStart(this.duration);
+    if (this.totalTimeInSeconds > 0) {
+      if (this.onStart && this.duration === this.totalTimeInSeconds)
+        this.onStart(this.duration);
       if (!this.running) {
         this.tick();
         this.timerID = setInterval(this.tick, 20);
@@ -47,7 +46,6 @@ class Timer {
     this.running = false;
   };
   tick = () => {
-
     if (this.durationInput.value === `00:00`) {
       this.pause();
       if (this.onComplete) this.onComplete();
@@ -58,12 +56,14 @@ class Timer {
       minutes = minutes < 0 ? 0 : minutes;
       let seconds = (this.duration % 60).toFixed(2);
       seconds = seconds < 0 ? 0 : seconds;
-      let result = this.duration > 0.02 ? `${minutes}: ${seconds < 10 ? (seconds = `0${seconds}`) : seconds}` : '00:00';
-      this.durationInput.value= result;
+      let result =
+        this.duration > 0.02
+          ? `${minutes}: ${seconds < 10 ? (seconds = `0${seconds}`) : seconds}`
+          : '00:00';
+      this.durationInput.value = result;
       if (this.onTick) {
         this.onTick(parseFloat(minutes * 60) + parseFloat(seconds));
       }
     }
   };
 }
-
